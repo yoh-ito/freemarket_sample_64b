df6e20079fb7194c0e2c476563c8bcad35499471
koyo-1008 <kou1088@yahoo.co.jp>
2020-03-07 14:31:00 +0900

    デプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '<用意したElastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '18.176.37.146', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
