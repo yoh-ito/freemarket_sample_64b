a2599347d53ad822c917bfad777e68f30710e6e3
poteto033 <nhnhnnhn033@gmail.com>
2020-03-16 12:19:02 +0900

    テンプレート化
    ・application.html.hamlに記述したものを削除した。
    ・header,footer,camera-btnのa(haref,"#")をlink_toに変更。
    ・show-mainを部分テンプレート化した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "items#show"
+  root "items#index"
+  resources :items, only: [:show]
 end
 
