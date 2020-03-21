e56c7bba771fd8cfc69f2abd6f67742a08efae87
yoh-ito <58355678+yoh-ito@users.noreply.github.com>
2020-03-21 21:28:16 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :items, only: [:show , :edit]
-  resources :users, only: [:new]
+  resources :users, only: [:new , :show]
+  resources :users_side, only: [:new , :show]
+  resources :cards, only: [:new]
 end
