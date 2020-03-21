603fa8f959ea45651164b699fee44dcf214d48ed
koyo-1008 <kou1088@yahoo.co.jp>
2020-03-21 16:02:21 +0900

    rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
   root "items#index"
-  resources :items, only: [:show , :edit]
+  resources :items, only: [:show , :edit, new]
   resources :users, only: [:new]
 end
