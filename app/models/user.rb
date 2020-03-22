341cfa07b2db62b70c9ed73f4cbf3b5cab46077b
ToshihiroKimura <toshi.kimura5903@gmail.com>
2020-03-22 14:21:35 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ApplicationRecord
 
   validates :last_name,           presence: true
   validates :first_name,          presence: true
-  validates :last_name_kana,      presence: true
-  validates :first_name_kana,     presence: true
+  validates :last_name_kana,      presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :first_name_kana,     presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :nickname,            presence: true
   validates :birthday,            presence: true
   validates :email,               presence: true, uniqueness: true
-  validates :encrypted_password,  presence: true
+  validates :encrypted_password,  presence: true, format: { with: /\A(?=.*[^\d])+/, allow_blank: true}
 
 end
