5ccb7abbf97681eca298a47a41071e59f082c493
ToshihiroKimura <toshi.kimura5903@gmail.com>
2020-03-22 14:43:15 +0900

    application_controllerの修正、prefixの使用、reCaptcha環境変数の使用

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = '6LdADuMUAAAAALJz0-cpkHLVqBDrJTDt3up5wJ4m'
-  config.secret_key = '6LdADuMUAAAAAMUAI2yeYOrIYq15wtKjuZOxR2f5'
+  config.site_key = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']   
 end
