e9e05df6a0c821e9e38388997cea1e61cd334a36
ToshihiroKimura <toshi.kimura5903@gmail.com>
2020-03-29 20:19:42 +0900

    before_actionの設定、エラーハンドリング、スタイル修正

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,31 +1,40 @@
 class AddressesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_address, only: [:show, :edit, :update]
   
   def new
     @address = Address.new
   end
 
   def create
-    Address.create(address_params)
-    redirect_to root_path
+    @address = Address.create(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
-    @address = Address.find(params[:id])
   end
 
   def edit
-    @address = Address.find(params[:id])
   end
 
   def update
-    address = Address.find(params[:id])
-    address.update(address_params)
-    redirect_to root_path
+    if Address.update(address_params)
+      render :show
+    else
+      render :edit
+    end
   end
 
   private
   def address_params
     params.require(:address).permit(:postcode, :prefecture, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
+  def set_address
+    @address = Address.find(params[:id])
   end
 end
