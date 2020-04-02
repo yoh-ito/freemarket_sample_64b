b6af4d814dc7e76faab6699179696db01073afb6
aya0625-cmd <ayaaya06250625@gmail.com>
2020-04-02 21:45:02 +0900

    activehashの導入&その他ビュー設定

--- db/migrate/20200323043305_add_delivery_area_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddDeliveryAreaToItem < ActiveRecord::Migration[5.0]
-  def change
-    add_column :items, :delivery_area, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200323045055) do
+ActiveRecord::Schema.define(version: 20200402122844) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20200323045055) do
   end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "url",        null: false
+    t.string   "image",      null: false
     t.integer  "item_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -34,19 +34,19 @@ ActiveRecord::Schema.define(version: 20200323045055) do
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                      null: false
-    t.text     "text",            limit: 65535,             null: false
-    t.string   "item_status",                               null: false
-    t.integer  "price",                                     null: false
-    t.string   "delivery_charge",                           null: false
-    t.string   "delivery_days",                             null: false
-    t.integer  "category_id",                               null: false
+    t.string   "name",                          null: false
+    t.text     "text",            limit: 65535, null: false
+    t.string   "item_status",                   null: false
+    t.integer  "price",                         null: false
+    t.string   "delivery_charge",               null: false
+    t.integer  "delivery_area",                 null: false
+    t.string   "delivery_days",                 null: false
+    t.integer  "category_id",                   null: false
     t.integer  "brand_id"
     t.integer  "solder_id"
     t.integer  "buyer_id"
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
-    t.integer  "delivery_area",                 default: 0, null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.index ["brand_id"], name: "index_items_on_brand_id", using: :btree
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
