10f13556abac8cb7f82040761c61f2bffa0dd60f
aya0625-cmd <ayaaya06250625@gmail.com>
2020-04-06 15:14:30 +0900

    カテゴリー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @category_parent = ["---"]
+    @category_parent= Category.where(ancestry: nil).each do |parent|
+    @category_parent<<parent.name
+    end
+  end
+
+  def category_children
+    @category_children = Category.find(params[:parent_name]).children
+  end
+
+  def category_grandchildren
+    @category_grandchildren = Category.find(params[:child_name]).children
   end
 
   def create
@@ -19,5 +31,13 @@ class ItemsController < ApplicationController
   def purchase
     
   end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name,:text,:item_status,:price,:delivery_area,:delivery_charge,:delivery_days,:brand_id,:category_id,images_attributes: [:image]).merge(solder_id: current_user.id)
+  end
+  
+
 end
 
---
+++ db/migrate/20200404130639_add_brand_id_to_item.rb
@@ -0,0 +1,5 @@
+class AddBrandIdToItem < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :items, :brand, foreign_key: true
+  end
+end
