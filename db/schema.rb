bba769a73ee630a7c0bd5b9e789b25ab5d83918b
aya0625-cmd <ayaaya06250625@gmail.com>
2020-04-13 19:05:12 +0900

    Merge branch 'send_feature' of https://github.com/aya0625-cmd/freemarket_sample_64b into send_feature

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,31 @@
 
 ActiveRecord::Schema.define(version: 20200404133122) do
 
+  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "postcode"
+    t.string   "prefecture"
+    t.string   "city"
+    t.string   "block"
+    t.string   "building"
+    t.string   "phone_number"
+    t.integer  "user_id",      null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
+  end
+
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",     null: false
+    t.string   "customer_id", null: false
+    t.string   "card_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -72,6 +93,7 @@ ActiveRecord::Schema.define(version: 20200404133122) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"
