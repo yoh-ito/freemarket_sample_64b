ceb3395ecd90277be19b71293ebe31a6a944c92c
koyo-1008 <kou1088@yahoo.co.jp>
2020-04-18 15:41:56 +0900

    画像投稿

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -16,7 +16,6 @@ $(function(){
     fileReader.onloadend = function() {
       var src = fileReader.result
       var html= `
-      <div>
         <img src="${src}">
         <div class="destroy">
           削除
@@ -29,14 +28,3 @@ $(function(){
     fileReader.readAsDataURL(file);
   });
 });
-  
-    var input_number = document.getElementById("price-form").value;
-    if (input_number >= 300 && input_number <= 9999999) {
-      input_number = "¥ " + input_number * 0.03
-    } else {
-      input_number = "ー"
-    }
-    document.getElementById("item_price_number").innerHTML = input_number;
-  };
-  
-
