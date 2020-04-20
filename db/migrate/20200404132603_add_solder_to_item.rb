4efccd971e28b2a2c8e5bb2675c55d1b37aba0c7
aya0625-cmd <ayaaya06250625@gmail.com>
2020-04-21 01:44:55 +0900

    key

--- db/migrate/20200404132603_add_solder_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddSolderToItem < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :items, :solder, foreign_key:{ to_table: :users }
-  end
-end
--- db/migrate/20200404133122_add_buyer_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddBuyerToItem < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :items, :buyer, foreign_key:{ to_table: :users }
-  end
-end
