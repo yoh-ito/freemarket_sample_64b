d657107352923cdb4f71b2d6a98a7fdcc4d03337
aya0625-cmd <ayaaya06250625@gmail.com>
2020-04-30 14:28:39 +0900

    【出品ページ】販売手数料のJS修正

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -2,10 +2,10 @@ $(function () {
   $('#item_price_number').text("ー");
   $('.input-form_price').keyup(function () {
     var number = String($(this).val());
-      if ((number >= 300) && (number <= 9999999)) {
-        $('#item_price_number').text("¥ " + Math.floor(number * 0.03));
-      } else {
-        $('#item_price_number').text("ー");
-      }
-   });
- })
+    if ((number >= 300) && (number <= 9999999)) {
+      $('#item_price_number').text("¥ " + Math.floor(number * 0.03));
+    } else {
+      $('#item_price_number').text("ー");
+    }
+  });
+});
