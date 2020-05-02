11574ebb04cbd68106a899d638f4f77e10a3a6c6
aya0625-cmd <ayaaya06250625@gmail.com>
2020-05-02 18:05:46 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20200404130015) do
     t.integer  "buyer_id"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "categories_fk_1", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -92,4 +94,5 @@ ActiveRecord::Schema.define(version: 20200404130015) do
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
   add_foreign_key "images", "items"
+  add_foreign_key "items", "categories", name: "categories_fk_1", on_update: :cascade, on_delete: :nullify
 end
